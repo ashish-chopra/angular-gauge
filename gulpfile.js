00b38d13a4d1a2ba3e1de2afaaa5a9efdcc1cfc4
Ashish Chopra <sendtoashishchopra@gmail.com>
2016-09-28 16:14:32 +0530

    initial version with bugs committed

--- gulpfile.js
+++ gulpfile.js
@@ -2,225 +2,107 @@
        File: gulpfile.js
        Author: Ashish Chopra
        Updated on: 21 may, 2016
-       -----------------------------------------
- 
-       gulpfile.js is a build automation script for front-end
-       web app project structure. It supports both development
-       and production level build lifecycle.
+       --------------------------------------
+       Gulpfile.js is customized for library development process for writing
+       front end libraries in JavaScript.
        
-       Development lifecycle supports copying assets, linting, CSS precompiling,
-       copying templates and HTML and live reloading on any changes.
-       
-       Prod lifecycle generates an optimized build in which code is minified, images are 
-       optimized etc.
-       
-       This build script can be used with following commands:
-       gulp                    -- default task used during development, switches on live reloading.
-       gulp clean              -- cleans the output of last build inside `dist` folder
-       gulp build:[options]    -- builds the code for defined options {dev | prod}.
-       gulp test               -- executes the test cases (coming soon)
-       gulp serve              -- It hosts the `dist` folder on a server at port 3000 (default)
-       
-       NOTE: Some commands mentioned above are not working right now. 
-       For more read the FUTURE WORK section. These are added on NEED BASIS.   
-    
-       FUTURE WORK
-       1. Support for eslinting.
-       2. Support for Unit Testing
-       3. Support for developing production ready builds.
-       4. Refactoring using gulp-load-plugins.
-       5. Adding a version.txt file during build generation. [Done]
-       6. Fixing linking errors of sourcemaps
-       7. JS loading using requireJS or browserify.
-       
+       TODO:
+       1. Adding the test framework support
+       2. Adding the code coverage support
+       3. Add the build assets support 
  */
 
+(function () {
+    'use strict';
 
+    var gulp = require('gulp'),
+        header = require('gulp-header'),
+        jshint = require('gulp-jshint'),
+        jscs = require('gulp-jscs'),
+        clean = require('gulp-clean'),
+        connect = require('gulp-connect'),
+        rename = require('gulp-rename'),
+        uglify = require('gulp-uglify'),
+        sourcemaps = require('gulp-sourcemaps'),
+        sequence = require('gulp-sequence'),
+        pkg = require('./package.json');
 
-/* IMPORT USED PACAKGES */
+    var port = 3000,
+        banner = ['/*!',
+        ' * <%= pkg.name %> - <%= pkg.description %>',
+        ' * <%= pkg.homepage %>',
+        ' * Version: <%= pkg.version %>',
+        ' *',
+        ' * Copyright 2016 Ashish Chopra',
+        ' * Released under the <%= pkg.license %> license',
+        ' * https://github.com/ashish-chopra/angular-gauge/blob/master/LICENSE',
+        ' */',
+        ''
+      ].join('\n');
 
-var gulp = require('gulp'),
-    clean = require('gulp-clean'),
-    connect = require('gulp-connect'),
-    mbf = require('main-bower-files'),
-    concat = require('gulp-concat'),
-    sass = require('gulp-sass'),
-    sourcemap = require('gulp-sourcemaps'),
-    autoprefixer = require('gulp-autoprefixer'),
-    runSequence = require('run-sequence'),
-    source = require('vinyl-source-stream'),
-    configs = require('./package.json');
-
-
-
-/* DEFINE PATH AND GLOBAL CONSTANTS */
-var port = 3000,
-    path = {
-        bower: 'bower_components',
-        scripts: "src/scripts/**",
-        templates: "src/templates/**",
-        scss: "src/scss/**/*.scss",
-        styles: "src/scss/**/*.scss",
-        index: "src/*.html",
-        assets: "src/assets/**",
-		data: "src/data/**",
-        vendor: 'dist/vendor',
-        css: 'dist/css',
-        dist: "dist",
-        root: "dist",
-        versionFile: 'version.txt'
-    },
-    sassOptions = {
-        errLogToConsole: true,
-        outputStyle: 'compressed'
-    },
-    autoprefixerOptions = {
-        browsers: ['last 2 versions']
-    },
-    BUILD_TYPE = {
-        PROD: "Production",
-        DEV: "Development"
-    },
-    buildType = BUILD_TYPE.PROD; // holds the type of build 'development' or 'production'
-
-
-/* DEFINING SUB TASKS */
-
-
-// copy the static content like images
-// and fonts in dist location
-gulp.task('statics', function () {
-    return gulp.src([path.assets], {
-            base: 'src'
-        })
-        .pipe(gulp.dest(path.dist))
-        .pipe(connect.reload());
-});
-
-// process the index html and templates
-gulp.task('html', function () {
-    return gulp.src([path.index, path.templates], {
-            base: 'src'
-        })
-        .pipe(gulp.dest(path.dist))
-        .pipe(connect.reload());
-});
-
-// process dummy data files 
-gulp.task('data', function () {
-    return gulp.src(path.data, {
-            base: 'src'
-        })
-        .pipe(gulp.dest(path.dist))
-        .pipe(connect.reload());
-});
-
-// process sass and generate autoprefixed CSS and sourcemaps
-// ERROR: sourcemaps linking is an issue in chromeDevTools. Fix needed.
-gulp.task('css', function () {
-    return gulp.src(path.scss, {
-            base: 'src/scss'
-        })
-        .pipe(sass.sync().on('error', sass.logError))
-        .pipe(sourcemap.init())
-        .pipe(sass(sassOptions))
-        .pipe(sourcemap.write({
-            includeContent: false,
-            sourceRoot: 'src/scss/'
-        }))
-        .pipe(sourcemap.init({
-            loadMaps: true
-        }))
-        .pipe(autoprefixer(autoprefixerOptions))
-        .pipe(sourcemap.write('./'))
-        .pipe(gulp.dest(path.css))
-        .pipe(connect.reload());
-
-});
-
-// process domain scripts
-gulp.task('js', function () {
-    return gulp.src(path.scripts, {
-            base: 'src'
-        })
-        .pipe(gulp.dest(path.dist))
-        .pipe(connect.reload());
-});
-
-//process vendor scripts and styles
-gulp.task('vendor', function () {
-    return gulp.src(mbf(), {
-            base: path.bower
-        })
-        .pipe(gulp.dest(path.vendor));
-});
-
-// watching the changes and fire up the tasks
-gulp.task('watch', function () {
-    // watching static content
-    gulp.watch(path.assets, ['statics']);
-    // watching html
-    gulp.watch([path.index, path.templates], ['html']);
-    //watching css
-    gulp.watch(path.styles, ['css']);
-    //watching scripts
-    gulp.watch(path.scripts, ['js']);
-	//watching data
-    gulp.watch(path.data, ['data']);
-
-});
-
-// connects the server at given port and root.
-// enables the live reloading.
-gulp.task('connect', function () {
-    return connect.server({
-        livereload: true,
-        root: path.root,
-        port: port
+    gulp.task('clean', function () {
+        return gulp.src('./dist/*', {
+                read: false
+            })
+            .pipe(clean());
     });
-})
 
-// tagging a version with the build just created
-gulp.task('version-tag', function () {
-    var template =  " --------------------- Build Details -------------------- \r\n" +
-                    " build type : " + buildType + "\r\n" +
-                    " created on : " + new Date() + "\r\n" +
-                    " version: " + configs.version + "\r\n" +
-                    " --------------------------------------------------------";
-    
-    var stream = source(path.versionFile);
-    stream.write(template);
-    stream.pipe(gulp.dest(path.dist));
-});
+    gulp.task('lint', function () {
+        return gulp.src('**/*.js')
+            .pipe(jshint())
+            .pipe(jshint.reporter());
+    });
 
-//cleans the dist location
-function cleanTask() {
-    return gulp.src(path.dist, {
-            read: false
-        })
-        .pipe(clean());
-}
+    gulp.task('style', function () {
+        return gulp.src('**/*.js')
+            .pipe(jscs());
+    });
 
-function defaultTask() {
-    runSequence('clean', 'common', 'connect', 'watch');
-}
+    gulp.task('bower', function () {
+        return gulp.src('src/angular-gauge.js')
+            .pipe(header(banner, {
+                pkg: pkg
+            }))
+            .pipe(gulp.dest('./dist'))
+            .pipe(gulp.dest('./examples/'));
+    });
 
-function buildDevTask() {
-    buildType = BUILD_TYPE.DEV;
-    runSequence('clean', 'common', 'version-tag');
-}
+    gulp.task('js', ['bower'], function () {
+        return gulp.src('src/angular-gauge.js')
+            .pipe(header(banner, {
+                pkg: pkg
+            }))
+            .pipe(rename('angular-gauge.min.js'))
+            .pipe(sourcemaps.init())
+            .pipe(uglify({
+                preserveComments: 'license'
+            }))
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest('./dist/'))
+            // .pipe(gulp.dest('./examples/'))
+            .pipe(connect.reload());
+    });
 
-function buildProdTask() {
-    buildType = BUILD_TYPE.PROD;
-    console.log('coming soon.');
-}
+    gulp.task('watch', function () {
+        gulp.watch('src/*.js', ['js']);
+        gulp.watch('./examples/**/*.*', ['reload'])
+    });
 
-/* DEFINE CUSTOM TASK CHAINING */
+    gulp.task('default', sequence('clean', 'js', 'connect', 'watch'));
 
-gulp.task('clean', cleanTask);
-gulp.task('default', defaultTask);
-gulp.task('build:dev', buildDevTask);
-gulp.task('build:prod', buildProdTask);
-gulp.task('common', ['statics', 'vendor', 'html', 'css', 'js', 'data']);
-gulp.task('test', []);
-gulp.task('serve', ['connect']);
+    // connects the server at given port and root.
+    // enables the live reloading.
+    gulp.task('connect', function () {
+        return connect.server({
+            livereload: true,
+            root: './examples/',
+            port: port
+        });
+    });
+
+    gulp.task('reload', function () {
+        return gulp.src('./examples/**/*.*')
+            .pipe(connect.reload());
+    })
+
+})();
---
+++ src/angular-gauge.js
@@ -0,0 +1,186 @@
+(function (angular) {
+    angular
+        .module('angularGauge', [])
+        .directive('ngGauge', gaugeMeterDirective);
+
+    gaugeMeterDirective.$inject = [];
+
+    function gaugeMeterDirective() {
+        var Gauge = function (element, options) {
+            this.element = element;
+            this.context = element.getContext('2d');
+            this.options = options;
+            console.log(this.options);
+            this.create();
+        };
+
+        Gauge.prototype.getRadius = function () {
+            var center = this.getCenter(),
+                radius = center.x - this.options.thick - 2;
+            return radius;
+        };
+        Gauge.prototype.drawBackground = function (start, end) {
+            var context = this.context,
+                center = this.getCenter(),
+                radius = this.getRadius();
+            context.beginPath();
+            context.arc(center.x, center.y, radius, start, end, false);
+            context.lineWidth = this.options.thick;
+            context.strokeStyle = "#eee";
+            context.stroke();
+
+        };
+        Gauge.prototype.addLabelByCanvas = function () {
+            var center = this.getCenter(),
+                context = this.context;
+            context.font = "32px 'Open Sans'";
+            context.textAlign = "center";
+            context.fillText("40 mph", center.x, center.y);
+            var c = context.measureText("40").width * 3 / 4 + 10;
+            context.font = "16px 'Open Sans'";
+            context.fillText("mph", center.x + c, center.y);
+            context.font = "12px 'Open Sans'";
+            context.fillText("Hello World", center.x, center.y + 20);
+
+        }
+        
+        Gauge.prototype.addLabel = function() {
+            
+        }
+        Gauge.prototype.create = function () {
+            var center = this.getCenter(),
+                movePerFrame = 0.0174532925;
+            if (this.options.type == 'semi') {
+                var head = Math.PI,
+                    tail = 2 * Math.PI;
+            } else if (this.options.type == 'full') {
+                var head = 1.5 * Math.PI,
+                    tail = 3.5 * Math.PI;
+            } else {
+                var head = 0.8 * Math.PI,
+                    tail = 2.2 * Math.PI;
+            }
+            var context = this.context,
+                value = this.options.value;
+            radius = this.getRadius();
+            this.drawBackground(head, tail);
+
+
+            var distance = tail - head;
+            tail = head + (distance * value) / 100;
+            console.log(tail);
+
+            var stripe = this.options.stripe || 2;
+            var thick = this.options.thick;
+
+            function animate() {
+                requestID = window.requestAnimationFrame(animate);
+
+                if (head <= tail) {
+                    context.beginPath();
+                    var newPos = head + 2 * stripe * movePerFrame;
+                    context.arc(center.x, center.y, radius, head, newPos, false);
+
+                    context.lineCap = 'round';
+                    context.lineWidth = thick;
+                    context.strokeStyle = "#ffcc66";
+                    context.stroke();
+                    head = newPos;
+
+                } else {
+                    cancelAnimationFrame(requestID);
+                }
+            }
+            console.log("stripe" + stripe);
+            this.addLabel();
+            animate();
+        };
+
+        Gauge.prototype.clear = function () {
+            var center = this.getCenter();
+            this.context.clearRect(0, 0, this.getWidth(), this.getHeight());
+        };
+
+        Gauge.prototype.getCenter = function () {
+            var x = this.context.canvas.width / 2,
+                y = this.context.canvas.height / 2;
+            return {
+                x: x,
+                y: y
+            };
+        }
+
+        Gauge.prototype.getWidth = function () {
+            return this.context.canvas.width;
+        }
+        Gauge.prototype.getHeight = function () {
+            return this.context.canvas.height;
+        }
+
+        Gauge.prototype.update = function (val) {
+            this.clear();
+            this.options.value = val;
+            this.create();
+        }
+
+        return {
+            restrict: 'E',
+            replace: true,
+            template: "<div class='parent'><span class='text'>121 GE</span><canvas height='180' width='180'></canvas></div>",
+            scope: {
+                value: "=",
+                used: "=",
+                total: "=",
+                type: "@",
+                size: "@",
+                thick: "@",
+                stripe: "@",
+                label: "@",
+                text: "@"
+            },
+            controller: function ($scope) {
+                var defaultOptions = {
+                    size: 180,
+                    thick: 2,
+                    type: 'full'
+                };
+                var vm = this;
+                vm.options = angular.extend({}, $scope);
+                console.log(vm.options);
+            },
+            link: function (scope, element, attrs, ctrl) {
+                if (scope.value && scope.value != '') {
+                    setTimeout(function () {
+                        initOrUpdateGauge();
+                    })
+                }
+
+                var gauge;
+
+                function initOrUpdateGauge() {
+                    if (!gauge) {
+                        var options = {
+                            type: ctrl.options.type,
+                            size: ctrl.options.size,
+                            value: ctrl.options.value,
+                            thick: ctrl.options.thick,
+                            stripe: ctrl.options.stripe
+                        };
+                        console.log(options.thick);
+                        gauge = new Gauge(element.find("canvas")[0], options);
+                        console.log(gauge);
+                    } else {
+                        // update gauge
+                    }
+                }
+
+                // monitor changes in directive scope
+                scope.$watch('value', function (newVal) {
+                    if (gauge)
+                        gauge.update(newVal);
+                });
+
+            }
+        }
+    }
+}(angular));
