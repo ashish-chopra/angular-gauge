e323e050f7394e90a80d664137d77032827ef8bb
Ashish Chopra <sendtoashishchopra@gmail.com>
2017-03-01 15:26:21 +0530

    material added and basic layout first version created

--- scripts/app.js
+++ scripts/app.js
@@ -2,10 +2,15 @@
     'use strict';
 
     angular
-        .module('websiteApp', [])
+        .module('websiteApp', ['ngMaterial', 'angularjs-gauge'])
+        .config(configApp)
         .controller('MainController', mainController);
 
-    
+    configApp.$inject = ['$mdThemingProvider'];
+    function configApp($mdThemingProvider) {
+       // $mdThemingProvider.disableTheming();
+    }
+
     mainController.$inject = [];
     function mainController() {
         var vm  = this;
