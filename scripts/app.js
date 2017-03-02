90003b6247530f8ca1e668e09946b5401622947b
Ashish Chopra <sendtoashishchopra@gmail.com>
2017-03-02 10:56:33 +0530

    teal theme added to ngMaterial

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,18 @@
 
     configApp.$inject = ['$mdThemingProvider'];
     function configApp($mdThemingProvider) {
-       // $mdThemingProvider.disableTheming();
+      $mdThemingProvider.theme('default')
+    .primaryPalette('teal', {
+      'default': '400', // by default use shade 400 from the pink palette for primary intentions
+      'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+      'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+      'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+    })
+    // If you specify less than all of the keys, it will inherit from the
+    // default shades
+    .accentPalette('amber', {
+      'default': '200' // use shade 200 for default, and keep all other shades the same
+    });
     }
 
     mainController.$inject = [];
